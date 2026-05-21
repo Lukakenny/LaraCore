7c9765f5d15d687c8d4c9703f4bfc30a25e0f522
Lukakeny <lukakeni95@gmail.com>
2026-05-22 00:33:01 +0200

    Add Post and Category models and migrations and controllers

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,16 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-
-
-
-
 Route::middleware('auth')->group(function () {
-    Route::get('admin', function () {
-        return view('admin');
-    })->name('admin');
-
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('admin', function () {return view('admin');})->name('admin');
 });
+
+
 
 require __DIR__.'/auth.php';
